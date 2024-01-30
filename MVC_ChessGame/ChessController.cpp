bb7a97f6552ca86c2d565aaee840931949c4cb63
Krzysztof Błażejewski <01161879@pw.edu.pl>
2024-01-30 11:02:06 +0100

    Adding docs

--- MVC_ChessGame/ChessController.cpp
+++ MVC_ChessGame/ChessController.cpp
@@ -2,78 +2,84 @@
 
 ChessController::ChessController()
 {
-	this->chessBoardWidget = new ChessBoardWidget(this);
-	this->chessGame = new ChessGame();
-	this->positionFromActive = false;
-	this->positionToActive = false;
-	connect(this->chessBoardWidget, SIGNAL(gameStarted()), this->chessGame, SLOT(startGame()));
-	connect(this->chessGame, SIGNAL(updateBoard(const QList<ChessPiece*>&)), this->chessBoardWidget, SLOT(updateBoard(const QList<ChessPiece*>&)));
-	connect(this->chessGame, &ChessGame::gameOver, this->chessBoardWidget, &ChessBoardWidget::showWinnerPanel);
-	connect(this->chessBoardWidget, &ChessBoardWidget::undoRequest, this->chessGame, &ChessGame::undoMove);
-	connect(this->chessGame, &ChessGame::promotionActive, this->chessBoardWidget, &ChessBoardWidget::showPromotionButtons);
-	connect(this->chessBoardWidget, &ChessBoardWidget::promoteToQueen, this, &ChessController::handlePromoteToQueen);
-	connect(this->chessBoardWidget, &ChessBoardWidget::promoteToBishop, this, &ChessController::handlePromoteToBishop);
-	connect(this->chessBoardWidget, &ChessBoardWidget::promoteToRook, this, &ChessController::handlePromoteToRook);
-	connect(this->chessBoardWidget, &ChessBoardWidget::promoteToKnight, this, &ChessController::handlePromoteToKnight);
+    // Initialization
+    this->chessBoardWidget = new ChessBoardWidget(this);
+    this->chessGame = new ChessGame();
+    this->positionFromActive = false;
+    this->positionToActive = false;
+
+    // Connect signals and slots
+    connect(this->chessBoardWidget, SIGNAL(gameStarted()), this->chessGame, SLOT(startGame()));
+    connect(this->chessGame, SIGNAL(updateBoard(const QList<ChessPiece*>&)), this->chessBoardWidget, SLOT(updateBoard(const QList<ChessPiece*>&)));
+    connect(this->chessGame, &ChessGame::gameOver, this->chessBoardWidget, &ChessBoardWidget::showWinnerPanel);
+    connect(this->chessBoardWidget, &ChessBoardWidget::undoRequest, this->chessGame, &ChessGame::undoMove);
+    connect(this->chessGame, &ChessGame::promotionActive, this->chessBoardWidget, &ChessBoardWidget::showPromotionButtons);
+    connect(this->chessBoardWidget, &ChessBoardWidget::promoteToQueen, this, &ChessController::handlePromoteToQueen);
+    connect(this->chessBoardWidget, &ChessBoardWidget::promoteToBishop, this, &ChessController::handlePromoteToBishop);
+    connect(this->chessBoardWidget, &ChessBoardWidget::promoteToRook, this, &ChessController::handlePromoteToRook);
+    connect(this->chessBoardWidget, &ChessBoardWidget::promoteToKnight, this, &ChessController::handlePromoteToKnight);
 }
 
 ChessBoardWidget* ChessController::getChessBoardWidget()
 {
-	return this->chessBoardWidget;
+    return this->chessBoardWidget;
 }
 
 void ChessController::handleBoxClicked(Position position)
 {
-	if (this->chessGame->getPieceAtPositionActivePlayer(position))
-	{
-		this->positionFrom = this->chessGame->getPieceAtPositionActivePlayer(position)->getPosition();
-		this->positionFromActive = true;
-		// do widgetu by podœwietli³o mo¿liwe pola
-		this->chessBoardWidget->markPossibleMoves(this->chessGame->getPieceAtPositionActivePlayer(position));
-		return;
-	}
-	//jesli ju¿ pozycja Z zosta³a ustalona to wykonaj ruch
-	if (positionFromActive)
-	{
-		this->positionTo = position;
-		this->positionToActive = true;
-		this->chessGame->makeMove(positionFrom, positionTo);
+    if (this->chessGame->getPieceAtPositionActivePlayer(position))
+    {
+        // If a piece is clicked, store its position and mark possible moves on the ChessBoardWidget.
+        this->positionFrom = this->chessGame->getPieceAtPositionActivePlayer(position)->getPosition();
+        this->positionFromActive = true;
+        this->chessBoardWidget->markPossibleMoves(this->chessGame->getPieceAtPositionActivePlayer(position));
+        return;
+    }
 
+    // If the source position is already set, execute the move.
+    if (positionFromActive)
+    {
+        this->positionTo = position;
+        this->positionToActive = true;
+        this->chessGame->makeMove(positionFrom, positionTo);
 
-		// wyzerowanie flag
-		this->positionFromActive = false;
-		this->positionToActive = false;
-	}
-
+        // Reset flags.
+        this->positionFromActive = false;
+        this->positionToActive = false;
+    }
 }
 
 void ChessController::cancelMove()
 {
-	this->positionFromActive = false;
-	this->positionToActive = false;
+    // Reset flags to cancel the current move.
+    this->positionFromActive = false;
+    this->positionToActive = false;
 }
 
 void ChessController::handlePromoteToQueen()
 {
-	this->chessGame->promotePawn(PieceType::Queen);
-	this->chessBoardWidget->hidePromotionButtons();
+    // Promote the pawn to a queen and hide the promotion buttons.
+    this->chessGame->promotePawn(PieceType::Queen);
+    this->chessBoardWidget->hidePromotionButtons();
 }
 
 void ChessController::handlePromoteToBishop()
 {
-	this->chessGame->promotePawn(PieceType::Bishop);
-	this->chessBoardWidget->hidePromotionButtons();
+    // Promote the pawn to a bishop and hide the promotion buttons.
+    this->chessGame->promotePawn(PieceType::Bishop);
+    this->chessBoardWidget->hidePromotionButtons();
 }
 
 void ChessController::handlePromoteToRook()
 {
-	this->chessGame->promotePawn(PieceType::Rook);
-	this->chessBoardWidget->hidePromotionButtons();
+    // Promote the pawn to a rook and hide the promotion buttons.
+    this->chessGame->promotePawn(PieceType::Rook);
+    this->chessBoardWidget->hidePromotionButtons();
 }
 
 void ChessController::handlePromoteToKnight()
 {
-	this->chessGame->promotePawn(PieceType::Knight);
-	this->chessBoardWidget->hidePromotionButtons();
+    // Promote the pawn to a knight and hide the promotion buttons.
+    this->chessGame->promotePawn(PieceType::Knight);
+    this->chessBoardWidget->hidePromotionButtons();
 }
-
--- MVC_ChessGame/ChessController.h
+++ MVC_ChessGame/ChessController.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "ChessGame.h"
 #include "ChessBoardWidget.h"
 #include <qobject.h>
@@ -6,27 +7,61 @@
 
 class ChessBoardWidget;
 
+/**
+ * @brief The ChessController class handles the communication between the ChessGame logic and the ChessBoardWidget GUI.
+ */
 class ChessController : public QObject
 {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	ChessController();
-	ChessBoardWidget* getChessBoardWidget();
-	void handleBoxClicked(Position position);
-	void cancelMove();
+    /**
+     * @brief Constructor for ChessController.
+     */
+    ChessController();
+
+    /**
+     * @brief Gets the ChessBoardWidget associated with the ChessController.
+     * @return A pointer to the ChessBoardWidget.
+     */
+    ChessBoardWidget* getChessBoardWidget();
+
+    /**
+     * @brief Handles the click event on a chessboard box.
+     * @param position The position of the clicked box.
+     */
+    void handleBoxClicked(Position position);
+
+    /**
+     * @brief Cancels the current move in progress.
+     */
+    void cancelMove();
 
 public slots:
-	void handlePromoteToQueen();
-	void handlePromoteToBishop();
-	void handlePromoteToRook();
-	void handlePromoteToKnight();
+    /**
+     * @brief Slot to handle promoting a pawn to a queen.
+     */
+    void handlePromoteToQueen();
+
+    /**
+     * @brief Slot to handle promoting a pawn to a bishop.
+     */
+    void handlePromoteToBishop();
+
+    /**
+     * @brief Slot to handle promoting a pawn to a rook.
+     */
+    void handlePromoteToRook();
+
+    /**
+     * @brief Slot to handle promoting a pawn to a knight.
+     */
+    void handlePromoteToKnight();
 
 private:
-	Position positionFrom;
-	bool positionFromActive;
-	Position positionTo;
-	bool positionToActive;
-	ChessGame* chessGame;
-	ChessBoardWidget* chessBoardWidget;
+    Position positionFrom; /**< Stores the position from which a piece is selected for a move. */
+    bool positionFromActive; /**< Flag indicating if a piece for the move has been selected. */
+    Position positionTo; /**< Stores the destination position for a move. */
+    bool positionToActive; /**< Flag indicating if the destination position for a move has been selected. */
+    ChessGame* chessGame; /**< Pointer to the ChessGame instance. */
+    ChessBoardWidget* chessBoardWidget; /**< Pointer to the associated ChessBoardWidget instance. */
 };
-
